f63e2462817f13d7b25c7cbdce8e421649cc3e23
Vanya <112898845+1TopOne1@users.noreply.github.com>
2023-02-09 16:13:01 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+//#1===============================
+
+console.log('Hello world!');
+
+//#2===============================
+
+console.log('Я вивчаю JS');
+
+//#3==============================
+
+const USER_COLOR_EYES = 'green';
+console.log(USER_COLOR_EYES);
+
+
+let user = '';
+let userName = 'Вася';
+
+user = userName;
+console.log(user);
+
+
+//4==============================
+
+
+
+
+
+
+
